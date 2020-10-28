9d244943d70fd03a37164613de3e00a3d4341156
marcosgusmao <marcosgusmao@hotmail.com>
2020-10-28 10:30:21 -0300

    tela login, components button e insert

--- dashspace/src/pages/signIn/index.tsx
+++ dashspace/src/pages/signIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -8,11 +11,11 @@ const SignIn: React.FC = () => (
     <Content>
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
+        <Input name="email" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input name="password" type="password" placeholder="Senha" />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
